bc6a9641a38c73ca199719492af63b4b8dc452ff
farrelfauzan298 <farrel@embreo.global>
2024-07-12 00:29:11 +0700

    [FIX] Redux Typing

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -34,7 +34,7 @@ export const store = configureStore({
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof reducer>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
 export const persistor = persistStore(store);
